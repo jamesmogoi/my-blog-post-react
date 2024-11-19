8406221c256d794431ba26d6a4222cab9dd5aa72
ezekielkibiego <kibiezekiel@gmail.com>
2024-11-19 13:04:05 +0300

    add images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@
 function About() {
     return (
         <div>
-          <h1>About the Blog</h1>
+          <h1 className="text-3xl font-bold underline">About the Blog</h1>
           <p>This blog is about my learning journey.</p>
         </div>
        );
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,8 +2,25 @@ import './Blog.css'
 
 function Blog() {
     const posts = [
-        {title: 'Learning React', content: 'React is a powerful JavaScript library for building user interfaces', author: 'John Doe'},
-        {title: 'Getting Started with Vite', content: 'Vite makes development fast and simple', author: 'Jane Smith'}
+        {
+            id: 1,
+            image: '/images/ferenc-almasi-c8h0n7fSTqs-unsplash.jpg',
+            title: 'Learning React', 
+            content: 'React is a powerful JavaScript library for building user interfaces', 
+            author: 'John Doe',
+            date: '2024-11-18',
+            time: '11.00 AM'
+        },
+           
+        {
+            id: 2,
+            image: '/images/lautaro-andreani-UYsBCu9RP3Y-unsplash.jpg',
+            title: 'Getting Started with Vite', 
+            content: 'Vite makes development fast and simple', 
+            author: 'Jane Smith',
+            date: '2024-11-18',
+            time: '11.00 AM'
+        }
     ]
     return (
         <div className='blog'>
@@ -11,9 +28,10 @@ function Blog() {
          <div className='grid'>
          {posts.map((post, index) => (
             <div key={index} className='card'>
+                <img src={post.image} alt={post.title}  className='w-full h-40 object-cover rounded-md mb-4'/>
                 <h2>{post.title}</h2>
                 <p>{post.content}</p>
-                <p><b>by {post.author}</b></p>
+                <p><b>by {post.author} </b> - {post.date} at {post.time}</p>
             </div>
          ))}
          </div>
